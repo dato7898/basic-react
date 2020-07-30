54786aad4995eafbd50676747cbd79d22c3eaec2
dato7898 <davidd7598@gmail.com>
2020-07-30 20:35:11 +0600

    Добавил отображение текста статьи по нажатию на кнопку

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react'
 import Article from './article'
 
 class ArticleList extends Component {
+	state = {
+		articleId: null
+	}
 	render() {
-		const articleElements = this.props.articles.map(article => <li><Article article = {article}/></li>)
 		return (
 			<ul>
-				{articleElements}
+				{this.body}
 			</ul>
+		)
+	}
+
+	toggleOpenArticle = (articleId) => () => this.setState({ articleId })
+
+	get body() {
+		return this.props.articles.map((article) => 
+			<li key = {article.id}>
+				<Article article = {article} 
+						 isOpen = {this.state.articleId === article.id}
+						 toggleOpen = {this.toggleOpenArticle(article.id)}/>
+			</li>
 		)
 	}
 }
