d2a320b0072269fa9f1230b53199c70233cfd297
dato7898 <davidd7598@gmail.com>
2020-07-30 22:38:47 +0600

    add react-select

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Select from 'react-select'
 import ArticleList from './components/article-list'
 import articles from './fixtures'
 
 class App extends Component {
+	state = {
+		openItem: null
+	}
 	render() {
 		return (
 			<div>
+				<Select options = {this.options} value={this.state.openItem} onChange={this.handleSelect} />
 				<ArticleList articles = {articles} />
 			</div>
 		)
 	}
+
+	get options() {
+		return articles.map(article => ({
+			label: article.title,
+			value: article.id
+		}))
+	}
+
+	handleSelect = openItem => this.setState({openItem})
 }
 
 export default App;
--- src/components/article.js
+++ src/components/article.js
@@ -7,13 +7,15 @@ class Article extends PureComponent {
 		return (
 			<div>
 				<div>
-					<h3>{article.title}</h3>
+					<h3 ref = {this.setTitleRef}>{article.title}</h3>
 					<button onClick = {this.handleBtnClick}>{isOpen ? 'close' : 'open'}</button>
 				</div>
 				{isOpen && <section>{article.text}</section>}
 			</div>
 		)
 	}
+
+	setTitleRef = (titleRef) => console.log(titleRef)
 
 	handleBtnClick = () => this.props.toggleOpen(this.props.article.id)
 }
