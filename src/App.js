092f8d1d9a5256ed674b71105c7ef7d2613d0f18
dato7898 <davidd7598@gmail.com>
2020-08-02 14:49:04 +0600

    add basic navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import ArticleList from './components/article-list'
+import { Route, NavLink } from 'react-router-dom'
 import UserForm from './components/user-form'
 import Filters from './components/filters/index'
 import Counter from './components/counter'
@@ -9,9 +10,26 @@ class App extends Component {
 		return (
 			<div>
 				<UserForm />
-				<Counter />
-				<Filters articles={[]} />
-				<ArticleList />
+				<div>
+					<div>
+						<NavLink to="/counter" activeStyle={{color: 'red'}}>
+							counter
+						</NavLink>
+					</div>
+					<div>
+						<NavLink to="/filters" activeStyle={{color: 'red'}}>
+							filters
+						</NavLink>
+					</div>
+					<div>
+						<NavLink to="/articles" activeStyle={{color: 'red'}}>
+							articles
+						</NavLink>
+					</div>
+				</div>
+				<Route path="/counter" component={Counter} />
+				<Route path="/filters" component={Filters} />
+				<Route path="/articles" component={ArticleList} />
 			</div>
 		)
 	}
