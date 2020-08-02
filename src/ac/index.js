3304d80b44d867e375ad5d51673292e602b0346e
dato7898 <davidd7598@gmail.com>
2020-08-02 21:33:33 +0600

    add connected-react-router

--- src/ac/index.js
+++ src/ac/index.js
@@ -1,3 +1,4 @@
+import { push } from 'connected-react-router'
 import { 
   INCREMENT, 
   DELETE_ARTICLE, 
@@ -63,7 +64,10 @@ export function loadArticleById(id) {
     })
 
     fetch(`/api/article/${id}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (res.status >= 400) throw new Error(res.statusText)
+        return res.json()
+      })
       .then((response) =>
         dispatch({
           type: LOAD_ARTICLE + SUCCESS,
@@ -71,13 +75,14 @@ export function loadArticleById(id) {
           response
         })
       )
-      .catch((error) =>
+      .catch((error) => {
         dispatch({
           type: LOAD_ARTICLE + FAIL,
           payload: { id },
           error
         })
-      )
+        dispatch(push('/error'))
+      })
   }
 }
 
---
+++ src/history.js
@@ -0,0 +1,9 @@
+// import {createBrowserHistory} from 'history'
+import createHistory from 'history/createBrowserHistory'
+
+const history = createHistory()
+
+//dev only
+window.routerHistory = history
+
+export default history
